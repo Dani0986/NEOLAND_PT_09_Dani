fc13bfd4ad06c404309ff9e8bad1a9123dbd6913
Dani0986 <daniosr1986@gmail.com>
2023-09-28 22:29:28 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
@@ -15,33 +15,48 @@ y = 10;
 let z;
 z = h + y;
 
-const character = {name: 'Jack sparrow', age: 10};
+console.log (z)
+
+const character = {name: 'Jack sparrow', age: 25};
 
 firstname = 'Jon'
 lastname = 'Snow'
 age = 24;
 
-console.log (firstname + lastname + age)
+console.log ("soy",  firstname, "tengo", lastname,  age + " y me gustan los lobos")
 
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
-console.log (toy1&toy2)
+
+console.log (toy1,toy2,19+29)
 
 let globalBasePrice = 25000
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 75000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 95000};
 
+
 10 * 5;
 
 10 / 2;
 
-15 / 9;  (1,6)
+15 / 9; // 1,6
 
-p = 10, j = 5, o = 15.
-
-i = 50, c = 10, m = 5.
-
-cosnole.log (15 / 9)
+p = 10;
+j = 5;
+o = p + j
 
 
+c = 10;
+m = 5;
+i = c * m
+
+console.log (10 * 5)
+
+console.log(10 / 2)
+
+console.log (15 / 9)
+
+console.log(o)
+
+console.log(i)
