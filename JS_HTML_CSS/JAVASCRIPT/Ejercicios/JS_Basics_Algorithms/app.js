0644f7ba89c98a04aeaa5d79604afb615018f081
Dani0986 <daniosr1986@gmail.com>
2023-09-28 20:48:16 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
@@ -1,6 +1,8 @@
 let myFavoriteHero; 
 myFavoriteHero = "Hulk"
 
+console.log (myFavoriteHero)
+
 let x;
 x = 50;
 
@@ -22,6 +24,24 @@ age = 24;
 console.log (firstname + lastname + age)
 
 const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcking', price: 29}
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log (toy1&toy2)
+
+let globalBasePrice = 25000
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 75000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 95000};
+
+10 * 5;
+
+10 / 2;
+
+15 / 9;  (1,6)
+
+p = 10, j = 5, o = 15.
+
+i = 50, c = 10, m = 5.
+
+cosnole.log (15 / 9)
 
 
