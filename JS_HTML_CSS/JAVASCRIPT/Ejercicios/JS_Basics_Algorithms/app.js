fa8058ddc97c2add7229e00a22bceb4ccc790203
Dani0986 <daniosr1986@gmail.com>
2023-09-28 20:22:57 +0200

    ejercicios

---
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
@@ -0,0 +1,27 @@
+let myFavoriteHero; 
+myFavoriteHero = "Hulk"
+
+let x;
+x = 50;
+
+let h;
+h = 5;
+
+let y;
+y = 10;
+
+let z;
+z = h + y;
+
+const character = {name: 'Jack sparrow', age: 10};
+
+firstname = 'Jon'
+lastname = 'Snow'
+age = 24;
+
+console.log (firstname + lastname + age)
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcking', price: 29}
+
+
