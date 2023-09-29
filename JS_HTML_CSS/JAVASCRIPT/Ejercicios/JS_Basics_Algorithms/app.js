ded7fc3bbf6110c0bd268d7dc741a98acb5fb7a7
Dani0986 <daniosr1986@gmail.com>
2023-09-29 20:55:42 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
@@ -64,7 +64,8 @@ const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters.push("Morty" , "Summer");
 console.log(rickAndMortyCharacters[4]);
 
-const rickAndMorty = ["Rick", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+const rickAndMorty = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMorty.splice(1,1);
 console.log(rickAndMorty);
 
 const number1 = 10;
@@ -78,3 +79,17 @@ if (number1 === 10){
 if (number1 / number2) {
     console.log("number2 dividido entre number1 es igual a 2")
 }
+
+if (number1 - number2) {
+    console.log("number1 es estrictamente distinto a number2");
+  }
+  
+  if (number3) {
+    console.log("number3 es distinto number1");
+  }
+  
+  if (number3 = 5) {
+    console.log("number3 por 5 es igual a number1");
+  }
+  
+ 
