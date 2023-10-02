01e59d6b5a0a75dbdfef2088614f0c67d456ee5f
Dani0986 <daniosr1986@gmail.com>
2023-10-02 19:04:49 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/app.js
@@ -12,7 +12,11 @@ let z = h + y;
 
 console.log (z)
 
-const character = {name: 'Jack sparrow', age: 25};
+const character = {name: 'Jack sparrow', age: 10};
+
+character.age = 25; 
+
+console.log(character.age)
 
 firstname = 'Jon'
 lastname = 'Snow'
@@ -72,24 +76,11 @@ const number1 = 10;
 const number2 = 20;
 const number3 = 2;
 
-if (number1 === 10){
+if(number1 === 10) {
     console.log('number1 es estrictamente igual a 10')
 }
 
-if (number1 / number2) {
-    console.log("number2 dividido entre number1 es igual a 2")
+if (number2 /= number1) {
+  console.log("number2 dividido entre number1 es igual a 2");
 }
 
-if (number1 - number2) {
-    console.log("number1 es estrictamente distinto a number2");
-  }
-  
-  if (number3) {
-    console.log("number3 es distinto number1");
-  }
-  
-  if (number3 = 5) {
-    console.log("number3 por 5 es igual a number1");
-  }
-  
- 
