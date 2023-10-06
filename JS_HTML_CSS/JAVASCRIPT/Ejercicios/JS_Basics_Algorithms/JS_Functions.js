251d50bfc3288e060cdd233df32b447799f9f16d
Dani0986 <daniosr1986@gmail.com>
2023-10-06 21:03:11 +0200

    Ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Functions.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Functions.js
@@ -1,3 +1,18 @@
-function sum(numberOne, numberTwo){
-
+function sum(a, b){
+    return a + b;
 } 
+
+console.log(sum(2,3));
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(array){
+
+}
+console.log(findLongestWord(avengers));
+
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  // insert code
+}
