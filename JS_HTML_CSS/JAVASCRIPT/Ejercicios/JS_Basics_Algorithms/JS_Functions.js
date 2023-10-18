8657fbbb2254658b19449c6f49ad4a37e6ca97c6
Dani0986 <daniosr1986@gmail.com>
2023-10-18 18:51:01 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Functions.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Functions.js
@@ -38,13 +38,67 @@ console.log(sumAll(numbers))
 
 
 const numeros = [1, 2, 3, 5, 45, 37, 58];
-function average(myArray) {
-   let i = 0, sum = 0, ArrayLen = myArray.length;
-   while (i < ArrayLen) {
-      sum = sum + myArray[i++];
+function average(myArray) {    
+    if (myArray.length === 0) {
+        return 0;
     }
-   return sum / ArrayLen;
+
+    let sum = 0;
+    for (let i = 0; i<myArray.length; i++){
+        sum += myArray[i];
+    }
+    
+    let A = sum / myArray.length;
+    return A;
+}
+let result = average(numeros);
+console.log(result);
+
+//--------------------- constante numeros con bucle while (hay que ir con cuidado)
+//   let i = 0, sum = 0, ArrayLen = myArray.length;
+//   while (i < ArrayLen) {
+ //     sum = sum + myArray[i++];
+//    }
+//   return sum / ArrayLen;
+//}
+
+//let a = average(numeros);
+//console.log(a)
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(numLetras) {
+    if (numLetras.lenght === 0 ){
+        return 0;
+    }
+
+    let todo = numLetras.reduce(function(acc, cadena) {
+   if (typeof cadena === "string") {                 
+      return acc + cadena.length;
+    } else {
+      return acc; 
+    }
+  }, 0);
+
+    let promedio = todo / numLetras.length;
+    return promedio;
+
 }
 
-let a = average(numeros);
-console.log(a)
+let promedio = averageWord(mixedElements)
+console.log(promedio)
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+
+ 
