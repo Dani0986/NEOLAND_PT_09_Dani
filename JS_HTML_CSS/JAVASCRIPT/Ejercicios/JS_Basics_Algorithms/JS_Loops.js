e96c290b553c807c4a1d2b31457741ea825e3779
Dani0986 <daniosr1986@gmail.com>
2023-10-22 16:14:57 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Loops.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Loops.js
@@ -66,6 +66,8 @@ for (let i = 0; i < placesToTravel.length; i++) {
   if (placesToTravel[i].id !== 11 && placesToTravel[i].id !== 40) {
     destinos.push(placesToTravel[i]);
   }
+
 }
+
 
 console.log(destinos);
