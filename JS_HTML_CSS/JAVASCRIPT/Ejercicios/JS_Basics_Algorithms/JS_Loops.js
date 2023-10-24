590759b6948f09e5d28187850d9ae5c72c86950d
Dani0986 <daniosr1986@gmail.com>
2023-10-24 19:03:56 +0200

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Loops.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_Loops.js
@@ -69,5 +69,43 @@ for (let i = 0; i < placesToTravel.length; i++) {
 
 }
 
-
 console.log(destinos);
+
+//?--- Iteración #6: Mixed For...of e includes
+//?--- Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función .includes() para comprobarlo
+
+const toys = [
+  {id: 5, name: 'Buzz MyYear'}, 
+  {id: 11, name: 'Action Woman'}, 
+  {id: 23, name: 'Barbie Man'}, 
+  {id: 40, name: 'El gato con Guantes'},
+  {id: 40, name: 'El gato felix'}
+  ]
+
+const filteredToys = []
+
+for (let mattel of toys) {
+  if(mattel.name.includes('gato')) {
+  } else {
+    filteredToys.push(mattel);
+  }
+}
+
+console.log(filteredToys);
+
+//?--- Iteración #7: For...of avanzado
+//?--- Usa un bucle for...of para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola...
+
+const popularToys = [];
+
+const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+for (let juguetes of toys2) {
+  console.log(juguetes.sellCount);  
+}
