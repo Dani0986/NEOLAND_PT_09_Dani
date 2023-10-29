f202263c8aa85fa661f9ac205052991150af6e12
Dani0986 <daniosr1986@gmail.com>
2023-10-29 11:31:42 +0100

    ejercicios

--- JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_ES6.js
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_Basics_Algorithms/JS_ES6.js
@@ -239,3 +239,19 @@ const mayores = streamers1.filter(streamer => {
 
 console.log("🚀 ~ file: JS_ES6.js:239 ~ mayores ~ mayores:", mayores);
 
+//!--- 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+//!--- los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+//!--- introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+//!--- introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+
+
+const streamers4 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const index = document.getElementById('index_ES6');
+
+
