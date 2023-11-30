f7a69b8dd5ec694b9531ed8f273a7c015c219d83
Dani0986 <daniosr1986@gmail.com>
2023-11-30 21:03:15 +0100

    ejercicios

---
+++ JS_HTML_CSS/JAVASCRIPT/Ejercicios/JS_DOM/Iteracion1/app.js
@@ -0,0 +1,28 @@
+//?- 1.1 Usa querySelector para mostrar por consola el botón con 
+//?- la clase .showme
+
+const button = document.querySelector(".showme");
+console.log("🚀 ~ file: main.js:6 ~  button:", button);
+
+//?- 1.2 Usa querySelector para mostrar por consola el h1 con el id 
+//?- #pillado
+
+const pillado = document.getElementById("pillado");
+console.log("🚀 ~ file: main.js:10 ~ pillado:", pillado);
+
+//?- 1.3 Usa querySelector para mostrar por consola todos los p
+
+const allP = document.querySelectorAll("p");
+console.log("🚀 ~ file: app.js:16 ~ allP:", allP);
+
+//?- 1.4 Usa querySelector para mostrar por consola todos los 
+//?- elementos con la clase.pokemon
+
+const pokemon = document.querySelectorAll(".pokemon");
+console.log("🚀 ~ file: app.js:22 ~ pokemon:", pokemon);
+
+//?- 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+//?- data-function="testMe".
+
+const data = document.querySelectorAll("[data-function]");
+console.log("🚀 ~ file: app.js:28 ~ data:", data);
