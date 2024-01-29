cdc4cb8f12f8f3db35714debb703c33541c1f9b7
Dani0986 <daniosr1986@gmail.com>
2024-01-29 20:57:23 +0100

    Ejercicios

--- NODE/Ejercicios/10-Controllers/src/api/models/Movie.model.js
+++ NODE/Ejercicios/10-Controllers/src/api/models/Movie.model.js
@@ -8,6 +8,7 @@ const MovieSchema = new mongoose.Schema(
     year: { type: Number, require: true },
     characters: [{ type: mongoose.Schema.Types.ObjectId, ref: "Character" }],
     released: { type: Boolean},
+    image: { type: String, required: false },
   },
   {
     timestamps: true,
--- NODE/Ejercicios/10-Controllers/src/api/routes/movie.routes.js
+++ NODE/Ejercicios/10-Controllers/src/api/routes/movie.routes.js
@@ -4,8 +4,9 @@ const {
     toggleCharacters,
 } = require("../controllers/Movie.controller");
 
+const { upload } = require("../../middleware/file.middleware")
 const MovieRouter = require("express").Router();
 
-MovieRouter.post("/create", createMovie);
+MovieRouter.post("/create", upload.single("image"), createMovie);
 MovieRouter.patch("/toggle/:id", toggleCharacters);
 module.exports = MovieRouter;
