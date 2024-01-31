31f90aa7902a8e8fac25a768a142e3182fc287b7
Dani0986 <daniosr1986@gmail.com>
2024-01-31 18:50:26 +0100

    Ejercicios

---
+++ NODE/Ejercicios/11-ModeloDatos_Cloudinary/src/api/routes/character.routes.js
@@ -0,0 +1,25 @@
+//!-------- nos traemos UPLOAD (función de multer para subida de ficheros)
+
+const { upload } = require("../../middleware/files.middleware");
+
+//!-------- IMPORTAMOS LOS CONTROLADORES
+
+const { create } = require("../controllers/Character.controllers");
+
+//!--------- CREAMOS UN ROUTER ESPECIFICO PARA CHARACTER --- al llamarlo disponemos de todas las rutas ---> será llamado en el index
+
+const express = require("express");
+
+const CharacterRouter = express.Router();
+
+//!--------- AÑADIMOS NUESTRAS RUTAS
+
+// En medio de la ruta y del controlador (funión create)
+// Se encuentra el middleware de subida de ficheros a cloudinary
+// multer tiene el metodo SINGLE para subir una sola imagen
+//* por el req.file va a recibir una clave que se llama image y con esa
+//  * clave quiero que el midddleware upload me lo suba a cloudinary para este disponible cuando entre
+//  * al controlador mediante la req.file.path ===> esto es igual a la URL de la imagen en cloudinary
+CharacterRouter.post("/create", upload.single("image"), create);
+
+module.exports = CharacterRouter;
---
+++ NODE/Ejercicios/12-Nodemailer/index.js
@@ -0,0 +1,64 @@
+//!------------ IMPORTACIONES
+const express = require("express");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+
+dotenv.config();
+
+//!------------ TRAEMOS VARIABLES DE ENTORNO
+
+const PORT = process.env.PORT;
+
+//!----------- CONFIGURAMOS SERVIDOR WEB
+
+const app = express();
+
+//!------------ CONFIGURAMOS EL ROUTER
+
+const router = express.Router();
+
+// definimos la función que gestiona este endpoint
+
+router.get("/sendNewMail", (req, res, next) => {
+  // traemos variables de entorno para enviar email
+  const EMAIL = process.env.EMAIL;
+  const PASSWORD = process.env.PASSWORD;
+
+  // Creamos la funcion que contiene el servicio y la autentificacion
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+    tls: {
+        // AÑADIR ESTA PARTE PARA QUE FUCNCIONES
+        rejectUnauthorized: false
+    }
+  });
+
+  // Creamos las diferentes opciones de nuestro mensaje
+
+  const mailOptions = {
+    from: EMAIL,
+    to: "daniosr1986@gmail.com",
+    subject: "Mensaje de confirmación",
+    text: "Todo ok 👌🏾",
+  };
+
+  // Enviamos el email
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return next(error); // La ejecución continua, pero notifica del error
+    } else {
+      res.status(200).json(`Email send, ${info.response}`);
+    }
+  });
+});
+
+app.use("/api/v1", router);
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
