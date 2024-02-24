cb6a110beed2c3e17feb83fe6686fe1d1e409207
Dani0986 <daniosr1986@gmail.com>
2024-02-24 20:51:15 +0100

    Ejercicios

--- NODE/Ejercicios/Proyecto_BackEnd/src/api/controllers/games.controller.js
+++ NODE/Ejercicios/Proyecto_BackEnd/src/api/controllers/games.controller.js
@@ -2,33 +2,62 @@ const Character = require("../models/Character.model");
 const Comment = require("../models/Comment.model");
 const Game = require("../models/Games.model");
 const User = require("../models/User.model");
-
+const { deleteImgCloudinary } = require("../../middleware/file.middleware");
 
 //!--------------------- POST - CREATE 
 
 const createGame = async (req, res, next) => {
-  console.log(req.body);
+  // guardamos la url de la imagen que se sube a cloudinary
+  // los archivos (imagen) --> req.file
+
+  let catchImg = req.file?.path;
+
+  console.log("req body", req.body);
+  console.log("req file", req.file);
+
   try {
+    //!-----> ACTUALIZAR INDEXES
+    // Los indexes de forman cuando la clave es unica
+    // Es importante por si es modificado posteriormente a la creacion del controlador
     await Game.syncIndexes();
 
-    // Creamos nueva instancia de Movie
+    // Creamos una nueva instancia de Character con los datos del body
+
     const newGame = new Game(req.body);
 
-    // Guardamos ese registro en la db
+    // Comprobamos si hay imagen para añadirla al Character creado
+    if (catchImg) {
+      newGame.image = catchImg;
+    } else {
+      // sino trae imagen la solicitud, le ponemos al character una imagen por defecto
+      newGame.image =
+        "https://res.cloudinary.com/dhkbe6djz/image/upload/v1689099748/UserFTProyect/tntqqfidpsmcmqdhuevb.png";
+    }
+
+    //!---------- GUARDAMOS EL CHARACTER CREADO
     const saveGame = await newGame.save();
 
-    // Si existe es que ha guardado de forma correcta --> 200
+    // Comprobamos si el character se ha guardado para lanzar una respuesta
     if (saveGame) {
+      //Si se ha guardado lanzamos una respuesta correcta con los datos del Character generados
       return res.status(200).json(saveGame);
     } else {
-      // Sino existe es que no se ha guardado --> 409
-      return res.status(409).json("No se ha podido crear game");
+      // si no se ha guardado hay un error y lo lanzamos en la respuesta
+      return res
+        .status(404)
+        .json("No se ha podido guaradar en la base de datos");
     }
   } catch (error) {
-    return res.status(409).json({
-      error: "Error en la creación de nuevo Game",
-      message: error.message,
-    });
+    //! -----> solo entramos aqui en el catch cuando ha habido un error
+    /** SI HA HABIDO UN ERROR -----
+     * Tenemos que borrar la imagen en cloudinary porque se sube antes de que nos metamos en
+     * el controlador---> porque es un middleware que esta entre la peticion del cliente y el controlador
+     */
+    // comprobar si hay imagen en req.file porqe si es asi se ha subido a cloudinary y hay borrarla
+
+    req.file?.path && deleteImgCloudinary(catchImg);
+    next(error);
+    return res.status(409).json("Error en el creado de game");
   }
 };
 
